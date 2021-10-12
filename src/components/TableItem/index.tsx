e038ba126237f534f45a1c336c5ae54550676e9b
yansntss <yansantos.dev@gmail.com>
2021-10-11 22:44:45 -0300

    setando campo categoria

--- src/components/TableItem/index.tsx
+++ src/components/TableItem/index.tsx
@@ -1,6 +1,7 @@
 import * as C from './styles'
 import {Item} from '../../types/Items'
 import { formateDate } from '../../helpers/dateFilter'
+import {categories} from '../../data/categories'
 
 type Props = { 
     item: Item
@@ -10,7 +11,7 @@ export const TableItem = ({item}: Props) => {
 return (
     <C.TableLine>
         <C.TableColumn>{formateDate(item.date)}</C.TableColumn>
-        <C.TableColumn>{item.category}</C.TableColumn>
+        <C.TableColumn>{categories[item.category].title}</C.TableColumn>
         <C.TableColumn>{item.title}</C.TableColumn>
         <C.TableColumn>R$ {item.value}</C.TableColumn>
     </C.TableLine>
