2286471905a22705eb2f293be49d7395e863bea0
yansntss <yansantos.dev@gmail.com>
2021-10-13 19:33:08 -0300

    Add receitas, despesas e balanço

--- src/components/InfoArea/index.tsx
+++ src/components/InfoArea/index.tsx
@@ -1,12 +1,16 @@
 import * as C from "./styles";
 import { formataCurrentMonth } from "../../helpers/dateFilter";
+import {ResumeItem} from '../ResumeItem/index'
 type Props = {
   currentMonth: string;
   onMonthChange: (newMonth: string) => void;
+  income: number;
+  expense: number
 };
 
-export const InfoArea = ({ currentMonth, onMonthChange }: Props) => {
-  const handlePrevMonth = () => {
+export const InfoArea = ({ currentMonth, onMonthChange, income, expense }: Props) => {
+ 
+    const handlePrevMonth = () => {
     let [year, month] = currentMonth.split("-");
     let currentDate = new Date(parseInt(year), parseInt(month) - 1, 1);
     currentDate.setMonth(currentDate.getMonth() - 1);
@@ -26,7 +30,12 @@ export const InfoArea = ({ currentMonth, onMonthChange }: Props) => {
         <C.MonthTitle> {formataCurrentMonth(currentMonth)} </C.MonthTitle>
         <C.MonthArrow onClick={handleNextMonth}>➡️</C.MonthArrow>
       </C.MonthArea>
-      <C.ResumeArea></C.ResumeArea>
+      <C.ResumeArea>
+
+          <ResumeItem title='Receitas'value={income}/>
+          <ResumeItem title='Despesas'value={expense}/>
+          <ResumeItem title='Balanço'value={income - expense}/>
+      </C.ResumeArea>
     </C.Container>
   );
 };
--- src/data/items.ts
+++ src/data/items.ts
@@ -3,5 +3,7 @@ export const items: Item[] = [
     { date: new Date(2021, 9, 5), category: 'food', title: 'McDonalds', value: 32.12 },
     { date: new Date(2021, 9, 15), category: 'food', title: 'Buger King', value: 28.12 },
     { date: new Date(2021, 9, 15), category: 'rent', title: 'Aluguel Apt', value: 2300 },
-    { date: new Date(2021, 10, 18), category: 'salary', title: 'Salário ACME', value: 4300 },
+    { date: new Date(2021, 9, 5), category: 'salary', title: 'Salário ACME', value: 4300 },
+    { date: new Date(2021, 11, 18), category: 'salary', title: 'Salário ACME', value: 300 },
+    { date: new Date(2021, 12, 18), category: 'salary', title: 'Salário ACME', value: 10000 },
 ];
